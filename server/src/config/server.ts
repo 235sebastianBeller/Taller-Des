6b2f187670e9b70428347e776e78778a7aae3482
235sebastianBeller <sebastian.beller@ucb.edu.bo>
2025-09-10 00:34:35 -0400

    Cleaned secrets

--- server/src/config/server.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-
-function server(app: Express, port: number): void {
-  app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-  });
-}
-
-export default server;
